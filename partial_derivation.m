5abe77a233aed3d7e39968de3de368e13c7afb6e
yhc <46378093+yhc1338@users.noreply.github.com>
2019-08-23 22:16:19 +0800

    Update partial_derivation.m

--- partial_derivation.m
+++ partial_derivation.m
@@ -1,15 +1,15 @@
 clc
 clear
 l=1000;
-x1=linspace(0,10,l);
-y1=linspace(0,10,l);
-dx=x1(2)-x1(1);
-dy=y1(2)-y1(1);
-z=zeros(l,l);
-x=zeros(l,l);
-y=zeros(l,l);
-fx=zeros(l,l);
-fy=zeros(l,l);
+x1=linspace(0,10,l);%x坐标
+y1=linspace(0,10,l);%y坐标
+dx=x1(2)-x1(1);%微元
+dy=y1(2)-y1(1);%微元
+z=zeros(l,l);%初值
+x=zeros(l,l);%初值
+y=zeros(l,l);%初值
+fx=zeros(l,l);%各点x方向偏导数  初值
+fy=zeros(l,l);;%各点y方向偏导数 初值
 for i=2:l
     z(1,i)=z(1,i-1)+(fx(1,i-1)+(4*x1(1,i-1)*y1(1))*dx)*dx;
     fx(1,i)=fx(1,i-1)+(4*x1(i-1)*y1(1))*dx;
