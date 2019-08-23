d0f73cb31b2aa0c042b5ae947d9a3196cacd2af3
yhc <46378093+yhc1338@users.noreply.github.com>
2019-08-23 22:32:15 +0800

    Update partial_derivation.m

--- partial_derivation.m
+++ partial_derivation.m
@@ -1,15 +1,15 @@
 clc
 clear
-l=1000;%离散分度
-x1=linspace(0,10,l);%x坐标
-y1=linspace(0,10,l);%y坐标
-dx=x1(2)-x1(1);%微元
-dy=y1(2)-y1(1);%微元
-z=zeros(l,l);%初值
-x=zeros(l,l);%初值
-y=zeros(l,l);%初值
-fx=zeros(l,l);%各点x方向偏导数  初值
-fy=zeros(l,l);;%各点y方向偏导数 初值
+l=1000; % step
+x1=linspace(0,10,l); % x axis
+y1=linspace(0,10,l); % y axis
+dx=x1(2)-x1(1); % delta x 
+dy=y1(2)-y1(1); % delta y 
+z=zeros(l,l); % initial
+x=zeros(l,l); % initial
+y=zeros(l,l); % initial
+fx=zeros(l,l); % partial derivative x and initial
+fy=zeros(l,l); % partial derivative y and initial
 for i=2:l
     z(1,i)=z(1,i-1)+(fx(1,i-1)+(4*x1(1,i-1)*y1(1))*dx)*dx;
     fx(1,i)=fx(1,i-1)+(4*x1(i-1)*y1(1))*dx;
@@ -27,7 +27,7 @@ for i=2:l
 end
 mesh(z)
 figure
-%对比
+% comparing
 for i=1:l
     for j=1:l
         z1(i,j)=x1(i)^2*y1(j)^2;
